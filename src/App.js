24e9507fdc680de5c74d8b1c8d4e30822c767e49
semirucucu <semirucucu@gmail.com>
2024-10-04 00:31:13 +0300

    counter-application

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, {useState} from "react";
+import "./App.css";
+
+const App = () => {
+    const [count, setCount] = useState(0);
+    return (
+        <div className="App">
+            <header>
+                <h1>Counter App using State/ Hooks</h1>
+            </header>
+            <h2>Current Value of count is {count}</h2>
+            
+            <button onClick={() => setCount(0)}>Reset Counter</button>
+            <button
+            onClick={() => {
+                setCount(count + 1);
+            }}
+            >
+                Increase Counter
+            </button>
+            <button
+            onClick={() => {
+                setCount(count - 1);
+            }}
+            >
+                Decrease Counter
+            </button>
+            
+        </div>
+    );
+};
+
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App/>);
